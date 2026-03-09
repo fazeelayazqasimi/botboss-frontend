cb1bb367fa860f8a650cfe9c9ee31174f86994ac
Fazeel Ayaz Qasimi <154905970+fazeelayazqasimi@users.noreply.github.com>
2026-03-09 11:03:41 +0500

    Update Interview.jsx

--- src/pages/Interview.jsx
+++ src/pages/Interview.jsx
@@ -354,7 +354,7 @@ const Interview = () => {
   const timerRef = useRef(null);
   const eyeCheckRef = useRef(null);
 
-  const API_URL = 'https://Fazeelayazq-botboss-backend.hf.space';
+  const API_URL = 'https://Fazeelayazqasimi-botboss-backend.hf.space';
 
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem('user'));
