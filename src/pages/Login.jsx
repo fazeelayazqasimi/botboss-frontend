33fd2eabcd401731019a9c85fb06daf4231ac28d
Fazeel Ayaz Qasimi <154905970+fazeelayazqasimi@users.noreply.github.com>
2026-03-28 02:26:34 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -246,7 +246,7 @@ const Login = () => {
                   <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"/>
                   <circle cx="12" cy="7" r="4"/>
                 </svg>
-                Candidate
+                Login as a Candidate
               </button>
               <button
                 type="button"
@@ -257,7 +257,7 @@ const Login = () => {
                   <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/>
                   <polyline points="9 22 9 12 15 12 15 22"/>
                 </svg>
-                Company
+                Login as a Company
               </button>
             </div>
 
